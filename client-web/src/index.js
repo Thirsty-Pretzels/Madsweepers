a30f390f1307a24966dcc8739ad324defd80e48e
Tim Zeng <zengtiantian1122@gmail.com>
2016-11-11 14:58:42 -0800

    initial setup of react redux.

--- client-web/src/index.js
+++ client-web/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
