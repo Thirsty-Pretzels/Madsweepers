209d56656e9a76d086487fc9150ecc69fa740241
Tim Zeng <zengtiantian1122@gmail.com>
2016-11-13 11:44:05 -0800

    Implement dynamic render of board based on status and val.

--- client-web/src/components/board_grid.js
+++ client-web/src/components/board_grid.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
-var Grid;
-export default Grid = ({grid, rowIndex, colIndex, playerLocation}) => (
-  <div
-    className='grid'>
-    { grid.val }
-  </div>
-)
+export default class Grid extends Component {
+  renderGrid() {
+
+    if (this.props.grid.status === 0) {
+
+      return (<img className='grid-initial'></img>);
+
+    } else if ( this.props.grid.status === 1 ) {
+
+      return (<img className='grid-flag'></img>);
+
+    } else if ( this.props.grid.status === 2 ) {
+
+      if ( this.props.grid.val === 9 ) {
+
+        return (<img className='grid-explosion'></img>);
+
+      } else {
+
+        return (<span className='grid-revealed'>{this.props.grid.val}</span>)
+      }
+    }
+  }
+
+  render() {
+    return (<div
+      className='grid'>
+      { this.renderGrid() }
+    </div>)
+  }
+}
 
 
--- client-web/src/reducers/reducer_player_location.js
+++ client-web/src/reducers/reducer_player_location.js
@@ -1,7 +1,6 @@
 export default function(state = [ {id:'player1', x: 0, y: 0},
       {id:'player2', x: 9, y: 9}, {id:'player3', x: 5, y: 5} ], action) {
 
-  var newPlayerLocation;
   if( action.type === 'ARROWUP' || action.type === 'ARROWDOWN' || action.type === 'ARROWLEFT' || action.type === 'ARROWRIGHT') {
     return action.payload;
   }
