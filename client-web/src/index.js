e4ba754e7f45e7c4306181d8a40e9a244b7630fe
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-11-13 11:09:47 -0800

    add comments

--- client-web/src/index.js
+++ client-web/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
+// import the customized middleware
 import startBoard, { boardMiddleware } from './middlewares/updateBoard';
 
+// define store
 const createStoreWithMiddleware = applyMiddleware(boardMiddleware)(createStore);
 const store = createStoreWithMiddleware(reducers);
+
+// initialize store
 startBoard(store);
 
 ReactDOM.render(
