c3ad306c75fdeb6e824a3121a7383e7d26514cd3
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-11-13 22:08:40 -0800

    add one more status for grid: player put a flag on a wrong spot

--- client-web/src/components/board_grid.js
+++ client-web/src/components/board_grid.js
@@ -4,15 +4,19 @@ export default class Grid extends Component {
   renderGrid() {
 
     if (this.props.grid.status === 0) {
-
+      // status 0: initial state
       return (<img className='grid-initial'></img>);
 
     } else if ( this.props.grid.status === 1 ) {
-
+      // status 1: player has put flag on a grid
       return (<img className='grid-flag'></img>);
 
-    } else if ( this.props.grid.status === 2 ) {
+    } else if ( this.props.grid.status === 3) {
+      // status 3: player put a flag on a wrong spot
+      return (<img className='wrong-flag'></img>);
 
+    } else if ( this.props.grid.status === 2 ) {
+      // status 2: the grid has been revealed
       if ( this.props.grid.val === 9 ) {
 
         return (<img className='grid-explosion'></img>);
