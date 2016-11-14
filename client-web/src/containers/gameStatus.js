23cbca25bab551ed9f7eeaa36105d85debad6e7e
meijiao <meijiao@gmail.com>
2016-11-14 12:07:17 -0800

    Game Status: Connect component to state

--- client-web/src/containers/gameStatus.js
+++ client-web/src/containers/gameStatus.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-//import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 
-export default class GameStatus extends Component {
+class GameStatus extends Component {
   render() {
     return (
       <div id='gameStatus'>
         <h2>Game Status</h2>
+        Mines Left: {this.props.mines}
       </div>
     )
   }
 }
 
-// var mapStateToProps = (state) => {
-//   return {
-//     mines: state.mines
-//   };
-// };
+var mapStateToProps = (state) => {
+  return {
+    mines: state.mines
+  };
+};
 
-//export default connect(mapStateToProps)(GameStatus);
+export default connect(mapStateToProps)(GameStatus);
