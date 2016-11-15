7fab8d8c9ac79fcd24e8928a2cdcb102ee8c18cc
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-11-14 17:08:06 -0800

    deal with conflicts

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ io.on('connection', function(socket){
   socket.on('CREATE-PLAYER', function(playerId) {
     playerId = playerId || ( '' + Math.floor( Math.random() * 10 ) );
     players.addPlayer(playerId);
-
+    clients.push({'socket': socket, 'playerId': playerId})
     io.emit('updatePlayerLocations', players.playerLocations);
   })
 
