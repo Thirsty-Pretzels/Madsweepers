e5ee657b06d9272dd6432fd275ab19838dc9cd96
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-11-15 14:55:45 -0800

    finissh progress bar

--- client-web/src/containers/gameStatus.js
+++ client-web/src/containers/gameStatus.js
@@ -5,8 +5,7 @@ class GameStatus extends Component {
   render() {
     return (
       <div id='gameStatus'>
-        <h2>Game Status</h2>
-        <text>  {this.props.minesInfo[1] - this.props.minesInfo[0]} / {this.props.minesInfo[1]} </text>
+        <h4>Game Status</h4>
         <progress 
           className={classNameDecider(this.props.minesInfo[1] - this.props.minesInfo[0], this.props.minesInfo[1])}
           value={this.props.minesInfo[1] - this.props.minesInfo[0]} 
@@ -20,15 +19,15 @@ class GameStatus extends Component {
 
 var classNameDecider = function(progress, total) {
   if (progress / total < 0.3) {
-    return 'progress progress-animated progress-striped progress-success';
-  } else if (progress / total < 0.55) {
-    return 'progress progress-animated progress-striped progress-info';
+    return 'progress progress-striped progress-success';
+  } else if (progress / total < 0.50) {
+    return 'progress progress-striped progress-info';
   } else if (progress / total < 0.8) {
-    return 'progress progress-animated progress-striped';
+    return 'progress progress-striped';
   } else if (progress / total < 0.92) {
-    return 'progress progress-animated progress-striped progress-warning';
+    return 'progress progress-striped progress-warning';
   } else {
-    return 'progress progress-animated progress-striped warning';
+    return 'progress progress-striped progress-danger';
   }
 }
 
