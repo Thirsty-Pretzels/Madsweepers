308c5a984876b5cd453d0f1c3fe0217cff5d24ec
meijiao <meijiao@gmail.com>
2016-11-14 19:16:23 -0800

    Fix merge conflict in index.js

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ function createBoard(rows, columns, dangerFactor) {
   rows = rows ? rows : 12;
   columns = columns ? columns : 12;
   dangerFactor = dangerFactor ? dangerFactor : 0.2;
+
   board = new Board();
   board.generate(rows, columns, dangerFactor);
   console.log('created newboard');
@@ -49,9 +50,6 @@ io.on('connection', function(socket){
 
   socket.on('GET-NEW-BOARD', function() {
     io.emit('updateBoard', board.board);  // to send stuff back to client side
-    // to send stuff back to client side
-    io.emit('updateBoard', board.board)
-    io.emit('countMines', board.minesLeft);
   });
 
   socket.on('CREATE-PLAYER', function(playerId) {
@@ -68,62 +66,6 @@ io.on('connection', function(socket){
 
   socket.on('DROP-FLAG', function(data){
     dropFlagHandler(io, board, data);
-    var playerId = data[0];
-    var location = data[1];
-
-    if (board.board[location.y][location.x].status === 0) {
-      board.board[location.y][location.x].status = 2;
-      //update the score according to the result;
-      if(board.board[location.y][location.x].val === 9) {
-        io.emit('updateScore', {id: playerId, scoreChange: scoreRevealMine});
-        board.minesLeft--;
-        io.emit('countMines', board.minesLeft);
-      } else {
-        io.emit('updateScore', {id: 'player'+playerId, scoreChange: scoreRevealspace});
-        board.todos--;
-      }
-      if (board.minesLeft === 0){
-        board.generate();
-        setTimeout(function(){
-          io.emit('updateBoard', board.board);
-        }, 300);
-      }
-    }
-    io.emit('updateBoard', board.board);
-  });
-
-  socket.on('DROP-FLAG', function(data){
-    var playerId = data[0];
-    var location = data[1];
-
-    if ( board.board[location.y][location.x].status === 0 ) {
-      // if the flag is dropped at a correct place
-      if ( board.board[location.y][location.x].val === 9 ) {
-        board.board[location.y][location.x].status = 1;
-        //update board
-        board.minesLeft--;
-        io.emit('countMines', board.minesLeft);
-        io.emit('updateBoard', board.board);
-        //update score
-        io.emit('updateScore', {id: 'player'+playerId, scoreChange: scoreRightFlag});
-      } else {
-      // if the flag is dropped at a wrong place
-        board.board[location.y][location.x].status = 3;
-        //update board
-        io.emit('updateBoard', board.board);
-        //update score
-        io.emit('updateScore', {id: 'player'+playerId, scoreChange: scoreWrongFlag});
-        //after 0.3, reset the corresponing grid to initial
-        setTimeout(() => {
-          board.board[location.y][location.x].status = 0;
-          io.emit('updateBoard', board.board);
-        }, 300);
-      }
-      if (board.minesLeft === 0){
-        board.generate();
-      }
-    }
-    console.log(board.todos, board.minesLeft);
   });
 
   socket.on('disconnect', function(){
