a0358c5d487d53b47aff67be5559ee7585f0e736
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-11-15 02:08:09 -0800

    make changes so that the system can show the correct scores dynamically

--- client-web/src/reducers/reducer_score_update.js
+++ client-web/src/reducers/reducer_score_update.js
@@ -1,19 +1,23 @@
 export default function(state = [], action) {
   if (action.type === 'UPDATE-SCORE') {
   	// state is only readable. Thus, can't revise state directly. Must generate a new State and revise this state;
-  	var newState = state.slice();
-    var foundUser = false;        //marker: if the action.scoreChange.id can be found in the state, set as true; or else default to be false;
-  	
-    for (var i = 0; i < newState.length; i++) {
-  	  if (newState[i].id === action.scoreChange.id) {
-  	  	newState[i].score += action.scoreChange.scoreChange;
-  	  	foundUser = true;
-        break;
-  	  }
-  	}
-    // if user can't be found in the state list, create a corresponding record in newState for it by pushing into newState;
-    if (!foundUser) {
-      newState.push({id: action.scoreChange.id, score: action.scoreChange.scoreChange});
+    var newState = [];
+
+    if (Array.isArray(action.scoreChange)) {
+      // if action.scoreChange is array, it means that one new player just joined the game 
+      for (let entry of action.scoreChange) {
+        newState.push({id: entry.id, score: entry.scoreChange});
+      }
+    } else {
+      // if action.scoreChange is not array, it means there is an update for an existing user
+      newState = state.slice();
+      
+      for (var i = 0; i < newState.length; i++) {
+        if (newState[i].id === action.scoreChange.id) {
+          newState[i].score += action.scoreChange.scoreChange;
+          break;
+        }
+      }
     }
 
   	return newState;
