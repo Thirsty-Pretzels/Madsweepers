d7e8362deb459a985014e82b11ef8d9b15a8d96c
Tim Zeng <zengtiantian1122@gmail.com>
2016-11-15 17:18:37 -0800

    Create some test for App component and LoginPage.

--- client-web/test/components/app_test.js
+++ client-web/test/components/app_test.js
@@ -14,4 +14,14 @@ describe('App' , () => {
   it('render the App component', () => {
     expect(component).to.exist;
   });
+
+  it('Check the title text', () => {
+    let DOMNode = findDOMNode(component);
+    expect(DOMNode.querySelector('h1').textContent).to.equal('This is the app component');
+  });
+
+  it('Should have a redirect function', () => {
+    expect(component.redirect).to.exist;
+  });
+
 });
