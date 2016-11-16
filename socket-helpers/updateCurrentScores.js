5bb205af904777a5212a6270b282eebfbdf5d219
AMinehan <soloin@gmail.com>
2016-11-16 11:54:44 -0800

    broadcast to room on game end

--- socket-helpers/updateCurrentScores.js
+++ socket-helpers/updateCurrentScores.js
@@ -1,12 +1,12 @@
 // helper function for dropFlagHandler and openSpaceHandler
 // update the score of a specific user in the currrentScores when there is change;
-module.exports = function(currentScores, scoreChange) {
+module.exports = function(currentScores, scoreChange, io, roomName) {
   for(var i = 0; i < currentScores.length; i++) {
   	if (currentScores[i].id === scoreChange.id) {
   	  currentScores[i].scoreChange += scoreChange.scoreChange;
       if (currentScores[i].scoreChange >= 100){
         console.log('max score reached')
-        //TODO: call end game function
+        io.to(roomName).emit('endification');
       }
   	  break;
   	}
