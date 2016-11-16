15d8760516409a7c9d544c38f8e709de84a07bb7
meijiao <meijiao@gmail.com>
2016-11-15 20:03:36 -0800

    Start writing bot (computer player)

---
+++ bot.js
@@ -0,0 +1,15 @@
+// var Bot = function () {
+//   this.username = 'Bot';
+//   this.room = '';
+
+// }
+
+// // Add a bot to the room 
+// Bot.prototype.start(username, room) {
+
+// }
+
+// Bot.prototype.makeMove() {
+//   socket.
+// }
+
