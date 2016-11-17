53a4916d4f0f65207769647b3587b5eb2d82a8eb
meijiao <meijiao@gmail.com>
2016-11-16 17:27:52 -0800

    Remove materialize, save basic CSS changes, and refactor app.js

--- client-web/src/components/app.js
+++ client-web/src/components/app.js
@@ -4,13 +4,6 @@ import Board from '../containers/board';
 import PlayGround from '../containers/playGround';
 import ScoreBoard from '../containers/scoreBoard';
 import GameStatus from '../containers/gameStatus';
-import injectTapEventPlugin from 'react-tap-event-plugin'; // For Material-UI 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
- 
-// Needed for onTouchTap 
-// http://stackoverflow.com/a/34015469/988941 
-injectTapEventPlugin();
-
 
 
 export default class App extends Component {
