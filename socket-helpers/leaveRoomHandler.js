2df8ea34221e82198c42c23ca6eb9524379a2607
Tim Zeng <zengtiantian1122@gmail.com>
2016-11-19 10:55:32 -0800

    Fix some game logics.

--- socket-helpers/leaveRoomHandler.js
+++ socket-helpers/leaveRoomHandler.js
@@ -14,4 +14,26 @@ module.exports = function(io, socket, room, user, gameManager, users, clients, c
 
   io.to(room).emit('roomInfoUpdate', gameManager.roomDetail(room, users));
   io.emit('roomListUpdate', gameManager.listRoom());
+
+  var isAllPlayersReady = Object.keys(gameManager.rooms[room].players.playerLocations).reduce(function(a, b) {
+    return a && gameManager.rooms[room].players.playerLocations[b].ready;
+  }, true);
+
+
+  if(isAllPlayersReady) {
+    // calculate board size
+    gameManager.startGame(room);
+
+    console.log('trying to restart the game for room ', room);
+
+    var boardSize = [gameManager.rooms[room].board.board[0].length, gameManager.rooms[room].board.board.length];
+
+    io.to(room).emit('updateBoard', {type: 0, board: gameManager.rooms[room].board.board});
+    io.emit('roomListUpdate', gameManager.listRoom());
+    io.to(room).emit('updateScore', gameManager.rooms[room].currentScores);
+    io.to(room).emit('updatePlayerLocations', {newLocations: gameManager.rooms[room].players.playerLocations, boardSize: boardSize});
+    io.to(room).emit('updateGameTime', gameManager.rooms[room].board.time);
+  }
+  io.to(room).emit('allPlayersReady', isAllPlayersReady);
+
 }
