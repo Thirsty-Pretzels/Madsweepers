0577814de066c8f456a6a8dc87c52353f22fb2d3
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-11-18 16:05:51 -0800

    finish the basic function of render roominfo

--- client-web/src/reducers/reducer_update_roominfo.js
+++ client-web/src/reducers/reducer_update_roominfo.js
@@ -1,6 +1,5 @@
 export default function(state = {host: null, userList: {}}, action) {
   if ( action.type === 'ROOM-INFO-UPDATE' ) {
-    console.log('inside room-info-update reducer: ', action.payload);
     return action.payload;
   }
 
