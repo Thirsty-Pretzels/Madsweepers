ab171e44bb3df5bf513c02fd3df64adf5853399f
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-11-29 13:29:03 -0800

    last failed trial

--- client-web/test/middleware/middleware_test.js
+++ client-web/test/middleware/middleware_test.js
@@ -1,9 +1,9 @@
-import io from 'socket.io';
+import io, { server }from 'socket.io';
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from "sinon-chai";
 import startBoard, { boardMiddleware } from '../../src/middlewares/updateBoard';
-import { WebSocket, Server, SocketIO } from 'mock-socket';
+import { SocketIO, Server } from 'mock-socket';
 
 var expect = chai.expect;
 chai.use(sinonChai);
@@ -16,12 +16,16 @@ var options ={
 
 
 describe('Test Suite for MiddleWare', () => {
-  let store, next, action, mockServer;
+  let store, next, action;
   let url = 'http://localhost:3000';
 
   beforeEach(() => {
     store = {};
     next = sinon.stub();
+  });
+
+  afterEach(() => {
+
   });
 
   it('action should be passed to next middleware to the next middleware', () => {
@@ -31,55 +35,22 @@ describe('Test Suite for MiddleWare', () => {
   });
 
   it('basic test: action should be passed', () => {
-    console.log('I am here');
+    var app = require('express')();
+    var http = require('http').Server(app);
+    var server = require('socket.io')(http);
 
-    mockServer = new Server(url);
-
-    mockServer.on('connection', (server, userSocket) => {
-      console.log('connection is established');
-      userSocket.on('loginTempUser', (data) => {
-        console.log(data);
-      });
+    server.on('connection', userSocket => {
+      console.log('someone connected.');
     });
 
-    global.socket = io(url);
+    http.listen(3000, function(){
+      console.log(`IAM listening on *:${port}, AMA`);
+    });
 
-    action = { type: 'LOGIN-TEMP-USER', payload: 'traderJoe'};
-    boardMiddleware(store)(next)(action);
+    global.socket = io.connect('http://localhost:3000');
+    // action = { type: 'LOGIN-TEMP-USER', payload: 'traderJoe'};
+    // boardMiddleware(store)(next)(action);
 
-    setTimeout(() => {
-      mockServer.stop();
-    }, 100);
+    http.close();
   });
 });
-
-// chat.js
-// function Chat() {
-//   const chatSocket = new WebSocket('ws://localhost:8080');
-//   this.messages = [];
-
-//   chatSocket.onmessage = (event) => {
-//     this.messages.push(event.data);
-//   };
-// }
-
-// describe('Chat Unit Test', () => {
-//   it('basic test', (done) => {
-//     const mockServer = new Server('ws://localhost:8080');
-//     mockServer.on('connection', server => {
-//       mockServer.send('test message 1');
-//       mockServer.send('test message 2');
-//     });
-
-//     // Now when Chat tries to do new WebSocket() it
-//     // will create a MockWebSocket object \
-//     var chatApp = new Chat();
-
-//     setTimeout(() => {
-//       const messageLen = chatApp.messages.length;
-//       assert.equal(messageLen, 2, '2 messages where sent from the s server');
-
-//       mockServer.stop(done);
-//     }, 100);
-//   });
-// });
