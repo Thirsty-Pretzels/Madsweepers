eb78854ee3a117228ae412243fc8b5ac657a8c15
meijiao <meijiao@gmail.com>
2016-12-01 10:57:45 -0800

    Activate redis database

--- client-web/src/containers/top_scores.js
+++ client-web/src/containers/top_scores.js
@@ -7,7 +7,7 @@ export class TopScores extends Component {
 
   componentWillMount() {
     var context = this;
-    //socket.emit('getHighScores');
+    socket.emit('getHighScores');
     socket.on('getHighScores', function (scores) {
       //format of data received from DB: ["Carol", "300", "Bob", "100", "mj", "2", "Alice", "1"]
       // highest score will be on front of the array
@@ -19,7 +19,7 @@ export class TopScores extends Component {
     var finalScores = this.props.scores; //finalScores: [ { id: 'mj', score: -15 } ]
     console.log(finalScores, 'component sending final scores')
     // save high scores in redis
-    //socket.emit('saveHighScores', finalScores);
+    socket.emit('saveHighScores', finalScores);
   }
 
   renderScore() {
@@ -35,10 +35,10 @@ export class TopScores extends Component {
 
   render() {
 
-    // // To uncomment when running db
-    // if (this.props.endification) {
-    //   this.saveHighScore();
-    // }
+    // To uncomment when running db
+    if (this.props.endification) {
+      this.saveHighScore();
+    }
 
     return (
       <div id='topScores'>
