e9ee118dd52a3bb1be0323eac41a9b52baa8fe1c
AMinehan <soloin@gmail.com>
2017-01-26 01:45:38 -0800

    fixes server crash

--- socket-helpers/leaveRoomHandler.js
+++ socket-helpers/leaveRoomHandler.js
@@ -15,12 +15,12 @@ module.exports = function(io, socket, room, user, gameManager, users, clients, c
   }, true);
 
   if (!playerList.length && !gameManager.rooms[room].defau) {
-    setTimeout(60000, function(){
+    setTimeout(function(){
       if (!playerList.length && !gameManager.rooms[room].defau) {
         delete gameManager.rooms[room];
         io.emit('roomListUpdate', gameManager.listRoom());
       }
-    })
+    }, 60000);
   }
 
   socket.emit('allPlayersReady', false);
